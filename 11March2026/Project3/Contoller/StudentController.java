bbd79ffb4d1f87cb4c56d9a4f39b5318453fe2bc
SanchanaSC <sanchu1549@gmail.com>
2026-03-11 14:05:49 +0530

    SpringBootDatabase

---
+++ 11March2026/Project3/Contoller/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.Project3.Contoller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Project3.Service.StudentService;
+import com.example.Project3.dto.request.StudentRequest;
+import com.example.Project3.dto.response.ApiResponse;
+import com.example.Project3.dto.response.StudentResponse;
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    private final  StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @PostMapping("/calc")
+    public ApiResponse getResult(@RequestBody StudentRequest request){
+        StudentResponse result=studentService.processStudent(request);
+        return new ApiResponse("Success", result);
+    }
+}
